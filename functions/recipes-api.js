0cbfd6e62851781bd3dd62bdecf08627b7869e1f
jwabednego <abednegojw@gmail.com>
2022-07-01 23:21:08 -0700

    CORS single recipe fix agian

--- functions/recipes-api.js
+++ functions/recipes-api.js
@@ -21,6 +21,9 @@ exports.handler = async (event, context) => {
       }
 
       return {
+        headers: {
+          "Access-Control-Allow-Origin": "*",
+        },
         statusCode: 200,
         body: JSON.stringify(recipes),
       };
